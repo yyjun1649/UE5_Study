2ec77607f84683dc9a7fd3ec8f20d4c7007e486e
yyjun1649 <yy164946@naver.com>
2023-01-26 13:48:50 +0900

    Init

---
+++ Source/UE5_3DGame/UE5_3DGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_3DGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_3DGame, "UE5_3DGame" );
---
+++ Source/UE5_3DGame/UE5_3DGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_3DGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_3DGAME_API AUE5_3DGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
