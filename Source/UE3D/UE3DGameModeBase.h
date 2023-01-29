5235140393b177a2e633e8b9cbdf69b00afac35d
yyjun1649 <yy164946@naver.com>
2023-01-29 20:09:06 +0900

    Character + Animation

---
+++ Source/UE3D/UE3DGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE3DGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE3D_API AUE3DGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
--- Source/UE5_3DGame/UE5_3DGame.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE5_3DGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_3DGame, "UE5_3DGame" );
